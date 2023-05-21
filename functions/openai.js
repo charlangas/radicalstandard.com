c884ab798eb0deb0b7e7f2a45327c33ad7a871a2
charlangas <charlangas@gmail.com>
2023-05-20 19:41:19 -0600

    Prepared for Netlify
    Removed API key from chatbot.js and readied other files for deployment on Netlify

---
+++ functions/openai.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { message } = JSON.parse(event.body);
+
+  const data = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: "You are a sales chatbot for an agency specialized in increasing productivity through AI." },
+      { role: 'user', content: `${message}` },
+    ],
+  };
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    });
+
+    const completion = response.data.choices[0].text.trim();
+    return { statusCode: 200, body: JSON.stringify({ message: completion }) };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: 'Error calling OpenAI API' }) };
+  }
+};
